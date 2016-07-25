efe77af22ffa03bc0b266134173e7a8298551719
Russ Otto <unplugged68@gmail.com>
2016-07-25 14:33:45 -0500

    Catchup commit
    I have absolutely no idea what exactly I changed from last time. Project sat too long. This is just an update to make sure that my local copy is synced with the github repo.

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,74 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    mainBowerFiles = require('main-bower-files'),
-    bower = require('main-bower-files'),
-    bowerNormalizer = require('gulp-bower-normalize'),
-    sourcemaps = require('gulp-sourcemaps'),
-    notify = require('gulp-notify'),
-    autoprefix = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    livereload = require('gulp-livereload');
+/*jshint esversion: 6, node: true */
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      autoprefix = require('gulp-autoprefixer'),
+      sourcemaps = require('gulp-sourcemaps'),
+      cleanCSS = require('gulp-clean-css'),
+      rename = require('gulp-rename'),
+      inject = require('gulp-inject'),
+      runSequence = require('run-sequence'),
+      livereload = require('gulp-livereload'),
+      del = require('del');
 
 var config = {
-  sassPath: './sass',
+  sassPath: './src/sass',
   bowerDir: './bower_components',
-  pubPath: './public'
+  pubPath: './dist'
 };
+
+gulp.task('clean', function() {
+  return del(['dist/index.html', 'dist/css', 'dist/maps']).then(paths => {
+    console.log('Files and directories deleted:/n', paths.join('\n'));
+  });
+});
 
 gulp.task('icons', function() {
   return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
     .pipe(gulp.dest(config.pubPath + '/fonts/'));
 });
 
-gulp.task('bower', function() {
-  return gulp.src(mainBowerFiles())
-    .pipe(gulp.dest('dist/lib'));
+gulp.task('sass', function() {
+  return gulp.src('src/sass/main.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass({
+        includePaths: 
+          [config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+           config.bowerDir + '/font-awesome/scss']
+      }))
+      .pipe(autoprefix())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('sass', function() {
-  return gulp.src(config.sassPath + '/**/*.scss')
-    .pipe(sourcemaps.init('.',{debug: true}))
-    .pipe(sass({
-      includePaths: [
-        config.sassPath,
-        config.bowerDir + '/bootstrap-sass/assets/stylesheets',
-        config.bowerDir + '/font-awesome/scss',
-        ]
-        })
-      .on("error", notify.onError(function (error) {
-        return "Error: " + error.message;
-        })))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.pubPath + '/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest(config.pubPath + '/css'));
+gulp.task('html', function() {
+  var injectFiles = gulp.src(['dist/css/main.css']);
+  var injectOptions = {
+    addRootSlash: false,
+    ignorePath: ['src', 'dist']
+  };
+
+  return gulp.src('./src/index.html')
+    .pipe(inject(injectFiles, injectOptions))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
-  app.use(express.static('public'));
+  app.use(express.static('dist'));
+  console.log('Starting localserver on port 4000\n');
   app.listen(4000, '0.0.0.0');
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('build', function() {
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['express', 'watch'], function() {
